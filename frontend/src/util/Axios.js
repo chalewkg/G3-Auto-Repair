f75cd99ca6a5987829c5a848e31ad045289e1f96
chalewkg <chalewkg@gmail.com>
2024-06-10 23:09:51 -0400

    updated url to server ip

--- frontend/src/util/Axios.js
+++ frontend/src/util/Axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export const axiosBase = axios.create({
-  baseURL: `http://localhost:8787/`,
+  baseURL: `http://3.140.186.242:8787/`,
 });
+
+
+// export const axiosBase = axios.create({
+//   baseURL: `http://localhost:8787/`,
+// });
